79279ff6bbe12c9702b2e85a535c102cfc19b0bc
YanG <valentinqoryaev@gmail.com>
2023-09-01 12:59:59 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let weight = 90;
+let height = 175;
+
+// console.log('Мой вес: ' + weight + ', мой рост: ' + height);
+    console.log(`Мой вес: ${weight}, мой рост ${height}`);
+    
+
+
+
+
+
